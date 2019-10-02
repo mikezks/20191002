1d792e7afb430e4dc22cae79e14f67e86c62b4b7
mikezks <michael.zikes@intauria.com>
2019-10-02 14:06:43 +0200

    RxJS

--- projects/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
+++ projects/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { timer, Observable, Subscription } from 'rxjs';
-import { tap, share } from 'rxjs/operators';
+import { timer, Observable, Subscription, throwError } from 'rxjs';
+import { tap, share, debounceTime, switchMap, filter } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
+import { Flight } from '@flight-workspace/flight-api';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-flight-typeahead',
@@ -10,10 +13,19 @@ import { tap, share } from 'rxjs/operators';
 export class FlightTypeaheadComponent implements OnInit, OnDestroy {
   timer$: Observable<number>;
   timerSubscription: Subscription;
+
+  control = new FormControl();
+  flights$: Observable<Flight[]>;
+  loading: boolean;
   
-  constructor() { }
+  constructor(private http: HttpClient) { }
   
-  ngOnInit() {
+  ngOnInit(): void {
+    //this.rxjsDemo();
+    this.initTypeahead();
+  }
+
+  rxjsDemo(): void {
     this.timer$ = timer(0, 1000)
       .pipe(
         tap(value => console.log('Implementierung wird durchlaufen', value)),
@@ -23,6 +35,32 @@ export class FlightTypeaheadComponent implements OnInit, OnDestroy {
     this.timerSubscription =
     this.timer$
       .subscribe(console.log);
+  }
+
+  initTypeahead(): void {
+    this.flights$ =
+      this.control
+        .valueChanges
+        .pipe(
+          debounceTime(300),
+          filter((from: string) => from.length > 2),
+          tap(() => this.loading = true),
+          switchMap(from => this.load(from)),
+          tap(() => this.loading = false),
+          tap(from => console.log('flights', from))
+        );
+  }
+
+  load(from: string): Observable<Flight[]>  {
+    let url = "http://www.angular.at/api/flight";
+
+    let params = new HttpParams()
+                        .set('from', from);
+
+    let headers = new HttpHeaders()
+                        .set('Accept', 'application/json');
+
+    return this.http.get<Flight[]>(url, {params, headers});
   }
   
   ngOnDestroy(): void {
